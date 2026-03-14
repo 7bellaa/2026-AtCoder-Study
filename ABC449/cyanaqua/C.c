489bcd1256bce9759d3eae491b11e04aab8eff3e
CyanAqua12 <111960875+CyanAqua12@users.noreply.github.com>
2026-03-14 21:16:46 +0900

    C: TLE

---
+++ ABC449/cyanaqua/C.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int stringLength, leftBound, rightBound;
+    char string[500100];
+
+    int numberOfPairs = 0;
+
+    scanf("%d %d %d", &stringLength, &leftBound, &rightBound);
+    scanf("%s", string);
+
+    for(int i = 0; i < stringLength; i++)
+    {
+        for(int j = leftBound; j <= rightBound; j++)
+        {
+            if(i + j >= stringLength) break;
+
+            if(string[i] == string[i + j])
+            {
+                numberOfPairs++;
+            }
+        }
+    }
+    printf("%d", numberOfPairs);
+    return 0;
+}
